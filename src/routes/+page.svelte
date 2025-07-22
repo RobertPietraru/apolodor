ed176acfb3dae207f7a647767c0264b22c620287
RobertPietraru <rob_piet@yahoo.com>
2025-07-22 17:40:09 +0300

    somewhat ok

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex flex-col md:items-start min-w-0">
+    <span class="font-bold text-lg md:text-xl truncate" style="font-family: 'Fredoka', sans-serif;">
+      Festivalul pentru Copii și Adolescenți Apolodor
+    </span>
+    <span class="text-sm md:text-base text-gray-500 mt-1 truncate">
+      18-21 septembrie 2025, Botoșani
+    </span>
+  </div>
